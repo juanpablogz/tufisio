a211dfe8cd82adc891a4c5d19baf4e5b232d8e74
juan pablo <juanpablo@MacBook-Pro-de-juan.local>
2020-10-26 16:46:20 -0500

    delete zoom

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
     title: 'landing',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
